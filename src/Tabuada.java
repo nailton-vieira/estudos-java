6d6e900498fcdba3d739e4713e3c4ef68777361b
nailton-vieira <nailtonvieira@gmail.com>
2024-02-12 14:26:52 -0300

    Adicionando class Tabuada

---
+++ src/Tabuada.java
@@ -0,0 +1,15 @@
+public class Tabuada {
+
+    public static void main(String[] args) {
+        int numero = 5;
+        
+
+        for (int multiplicador = 0; multiplicador <= 10; multiplicador++ ){
+
+            System.out.println(numero + "x" + multiplicador);
+
+        }
+
+    }
+    
+}
