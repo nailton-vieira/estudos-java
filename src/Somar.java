77c94a9297976d090bdf9ab9da130ab670107ae6
nailton-vieira <nailtonvieira@gmail.com>
2024-06-26 22:14:56 -0300

    Adicionando a class Somar ao repo

--- src/Somar.java
+++ src/Somar.java
@@ -1,8 +1,8 @@
 public class Somar {
          
-        public static double somando(double valor1, double valor2) {
+        public static double somando(double numero1, double valor2) {
 
-            double resultado = valor1 + valor2;
+            double resultado = numero1 + valor2;
 
             return resultado ;
 
