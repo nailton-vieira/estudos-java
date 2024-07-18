f4dc6fea535990a48d55e03581f69ea6b0973a8d
nailton-vieira <nailtonvieira@gmail.com>
2024-07-18 10:59:49 -0300

    Enviando estudo metodo construtor new Readme

---
+++ src/Pessoa.java
@@ -0,0 +1,17 @@
+public class Pessoa {
+
+    private String nome;
+    private String email;
+
+
+    public Pessoa(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+    
+    
+
+    
+
+
+}
