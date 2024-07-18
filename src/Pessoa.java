0d9e2aea9f0717312d1d520964b8fca2ff00eb65
nailton-vieira <nailtonvieira@gmail.com>
2024-07-18 15:56:47 -0300

    Adicionando metodo toString ao repo

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -8,9 +8,35 @@ public class Pessoa {
         this.nome = nome;
         this.email = email;
     }
-    
+
+
+public String getNome() {
+
+    return nome;
+
+}
+
+public String getEmail() {
+
+    return email;
+
+}
+
+public void setNome(String nome) {
+
+    this.nome = nome;
+}
+
+public void setEmail(String email) {
+
+    this.nome = email;
+}
     
 
+public String toString(){
+
+    return "Nome: " + this.nome + ", Email: " + this.email;
+}
     
 
 
--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,15 @@
 public class Principal {
 
     public static void main(String[] args) {
+
+        Pessoa funcionario = new Pessoa(null, null);
+
+        funcionario.setNome("Marcos");
+        funcionario.setEmail("teste");
+            
+
+
+           System.out.println(funcionario);
         
     }
 
